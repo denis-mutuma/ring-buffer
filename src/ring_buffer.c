8165ba33a08fe5b4f551f8710036893ab7070a75
denis-mutuma <mutuma@mutuma-pc>
2023-04-03 13:58:41 +0300

    add memory cleanup methods

--- src/ring_buffer.c
+++ src/ring_buffer.c
@@ -3,17 +3,50 @@
 #include <stdbool.h>
 #include "../include/ring_buffer.h"
 
-void InitBuffer(RingBuffer *ring_buffer, uint8_t *data, size_t capacity)
+RingBuffer *CreateRingBuffer(size_t capacity)
 {
-    ring_buffer->head = 0;
-    ring_buffer->tail = 0;
-    ring_buffer->capacity = capacity;
-    ring_buffer->length = 0;
-    ring_buffer->data = data;
-    return;
+    RingBuffer *buffer = (RingBuffer *)malloc(sizeof(RingBuffer));
+    buffer->data = malloc(sizeof(uint8_t) * capacity);
+
+    buffer->head = 0;
+    buffer->tail = 0;
+    buffer->capacity = capacity;
+    buffer->length = 0;
+
+    return buffer;
 }
 
-void ReadBuffer(RingBuffer *_this, RingBufferValue *result)
+void DeleteRingBuffer(RingBuffer *buffer)
+{
+    if (!buffer)
+    {
+        return;
+    }
+    free(buffer->data);
+    buffer->data = NULL;
+    free(buffer);
+    buffer = NULL;
+}
+
+RingBufferResult *CreateRingBufferResult()
+{
+    RingBufferResult *result = (RingBufferResult *)malloc(sizeof(RingBufferResult));
+    result->is_valid = false;
+    result->value = 0;
+    return result;
+}
+
+void DeleteBufferResult(RingBufferResult *result)
+{
+    if (!result)
+    {
+        return;
+    }
+    free(result);
+    result = NULL;
+}
+
+void ReadBuffer(RingBuffer *_this, RingBufferResult *result)
 {
     if (IsBufferEmpty(_this))
     {
@@ -28,7 +61,7 @@ void ReadBuffer(RingBuffer *_this, RingBufferValue *result)
     return;
 }
 
-bool WriteBuffer(RingBuffer *_this, const uint8_t data)
+bool WriteBuffer(RingBuffer *_this, uint8_t data)
 {
     if (IsBufferFull(_this))
     {
@@ -36,43 +69,39 @@ bool WriteBuffer(RingBuffer *_this, const uint8_t data)
     }
     _this->data[_this->head] = data;
     _this->length++;
-    if (IsBufferFull(_this))
-    {
-        return true;
-    }
     _this->head = (_this->head + 1) % _this->capacity;
 
     return true;
 }
 
-size_t GetBufferLength(const RingBuffer *ring_buffer)
+size_t GetBufferLength(RingBuffer *ring_buffer)
 {
     return ring_buffer->length;
 }
 
-size_t GetReadIndex(const RingBuffer *ring_buffer)
+size_t GetBufferReadIndex(RingBuffer *ring_buffer)
 {
     return ring_buffer->tail;
 }
 
-size_t GetWriteIndex(const RingBuffer *ring_buffer)
+size_t GetBufferWriteIndex(RingBuffer *ring_buffer)
 {
     return ring_buffer->head;
 }
 
-bool IsBufferFull(const RingBuffer *_this)
+bool IsBufferFull(RingBuffer *_this)
 {
-    return _this->length == _this->capacity;
+    return (_this->head == _this->tail && _this->length > 0);
 }
 
-bool IsBufferEmpty(const RingBuffer *_this)
+bool IsBufferEmpty(RingBuffer *_this)
 {
-    return _this->head == _this->tail;
+    return (_this->head == _this->tail && _this->length == 0);
 }
 
-void Reset(RingBuffer *ring_buffer)
+void RingBufferReset(RingBuffer *ring_buffer)
 {
-    for (size_t i = 0; i < ring_buffer->capacity; i++)
+    for (size_t i = 0; i < ring_buffer->capacity; ++i)
     {
         ring_buffer->data[i] = 0;
     }
---
+++ test/test.c
@@ -0,0 +1,116 @@
+#include "lib/Unity/src/unity.h"
+#include "../include/ring_buffer.h"
+
+void setUp(void) {}
+
+void tearDown(void) {}
+
+void test_CreateRingBuffer(void)
+{
+    RingBuffer *buffer = CreateRingBuffer(3);
+    TEST_ASSERT_NOT_NULL(buffer);
+    TEST_ASSERT_EQUAL_INT(buffer->capacity, 3);
+    TEST_ASSERT_EQUAL_INT(GetBufferLength(buffer), 0);
+    TEST_ASSERT_EQUAL_INT(GetBufferReadIndex(buffer), 0);
+    TEST_ASSERT_EQUAL_INT(GetBufferWriteIndex(buffer), 0);
+    TEST_ASSERT_FALSE(IsBufferFull(buffer));
+    TEST_ASSERT_TRUE(IsBufferEmpty(buffer));
+
+    DeleteRingBuffer(buffer);
+}
+
+void test_CreateRingBufferResult(void)
+{
+    RingBufferResult *result = CreateRingBufferResult();
+    TEST_ASSERT_NOT_NULL(result);
+    TEST_ASSERT_EQUAL_INT(result->value, 0);
+    TEST_ASSERT_FALSE(result->is_valid);
+
+    DeleteBufferResult(result);
+}
+
+void test_WriteRingBuffer(void)
+{
+    RingBuffer *buffer = CreateRingBuffer(3);
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 10));
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 20));
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 30));
+
+    TEST_ASSERT_EQUAL_INT(GetBufferLength(buffer), 3);
+    TEST_ASSERT_EQUAL_INT(GetBufferReadIndex(buffer), 0);
+    TEST_ASSERT_EQUAL_INT(GetBufferWriteIndex(buffer), 0);
+    TEST_ASSERT_FALSE(WriteBuffer(buffer, 40));
+    TEST_ASSERT_EQUAL_INT(GetBufferWriteIndex(buffer), 0);
+    TEST_ASSERT_EQUAL_INT(GetBufferReadIndex(buffer), 0);
+    TEST_ASSERT_TRUE(IsBufferFull(buffer));
+    TEST_ASSERT_FALSE(IsBufferEmpty(buffer));
+
+    DeleteRingBuffer(buffer);
+}
+
+void test_ReadRingBuffer(void)
+{
+    RingBuffer *buffer = CreateRingBuffer(3);
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 10));
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 20));
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 30));
+
+    RingBufferResult *result = CreateRingBufferResult();
+    ReadBuffer(buffer, result);
+    TEST_ASSERT_TRUE(result->is_valid);
+    TEST_ASSERT_EQUAL_INT(result->value, 10);
+    TEST_ASSERT_EQUAL_INT(GetBufferLength(buffer), 2);
+    TEST_ASSERT_EQUAL_INT(GetBufferReadIndex(buffer), 1);
+    TEST_ASSERT_EQUAL_INT(GetBufferWriteIndex(buffer), 0);
+
+    ReadBuffer(buffer, result);
+    TEST_ASSERT_TRUE(result->is_valid);
+    TEST_ASSERT_EQUAL_INT(result->value, 20);
+
+    ReadBuffer(buffer, result);
+    TEST_ASSERT_TRUE(result->is_valid);
+    TEST_ASSERT_EQUAL_INT(result->value, 30);
+
+    TEST_ASSERT_EQUAL_INT(GetBufferWriteIndex(buffer), 0);
+    TEST_ASSERT_EQUAL_INT(GetBufferReadIndex(buffer), 0);
+
+    TEST_ASSERT_TRUE(IsBufferEmpty(buffer));
+    TEST_ASSERT_FALSE(IsBufferFull(buffer));
+
+    ReadBuffer(buffer, result);
+    TEST_ASSERT_FALSE(result->is_valid);
+    TEST_ASSERT_EQUAL_INT(result->value, 0);
+
+    TEST_ASSERT_TRUE(IsBufferEmpty(buffer));
+    TEST_ASSERT_FALSE(IsBufferFull(buffer));
+
+    DeleteRingBuffer(buffer);
+    DeleteBufferResult(result);
+}
+
+void test_RingBufferReset(void)
+{
+    RingBuffer *buffer = CreateRingBuffer(3);
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 10));
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 20));
+    TEST_ASSERT_TRUE(WriteBuffer(buffer, 30));
+
+    RingBufferReset(buffer);
+
+    for (int i = 0; i < buffer->capacity; ++i)
+    {
+        TEST_ASSERT_EQUAL_INT(buffer->data[i], 0);
+    }
+
+    DeleteRingBuffer(buffer);
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_CreateRingBuffer);
+    RUN_TEST(test_WriteRingBuffer);
+    RUN_TEST(test_ReadRingBuffer);
+    RUN_TEST(test_CreateRingBufferResult);
+    return UNITY_END();
+}
